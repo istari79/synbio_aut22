cbc21322219312100acf3458b225e84cea1211de
istari79 <92231197+istari79@users.noreply.github.com>
2022-10-07 15:13:51 -0700

    Update RandomJavaFile.java

--- RandomJavaFile.java
+++ RandomJavaFile.java
@@ -1,6 +1,7 @@
 public class RandomJavaFile {
     public static void main(String[] args) {
         System.out.println("This is a test document to pull.");
+        System.out.println("Here is a change made in JGrasp.");
     }
 
 }
